d6176eb8675e9b7ec1ac36105119c8e92f5a42e7
Paulo Henrique Reis <phdsreis@gmail.com>
2026-01-26 20:44:57 -0300

    Adicionando a primeira migração com Alembic. Criada tabela de usuarios.

--- fastapi_zero/app.py
+++ fastapi_zero/app.py
@@ -59,12 +59,12 @@ def update_user(user_id: int, user: UserSchema):
     '/users/{user_id}', status_code=HTTPStatus.OK, response_model=UserPublic
 )
 def delete_user(user_id: int):
-    # del database[user_id-1]
     if user_id < 1 or user_id > len(database):
         raise HTTPException(
             status_code=HTTPStatus.NOT_FOUND, detail='User not found.'
         )
 
+    # del database[user_id-1]
     return database.pop(user_id - 1)
 
 
